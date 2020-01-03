af4849757d646997e8ca9311c5d91ddc303113c4
Nadia-Kadri <nadiagrace96@gmail.com>
2020-01-03 10:17:10 -0500

    mysql schema

---
+++ seeds.sql
@@ -0,0 +1,13 @@
+USE bamazon;
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Echo Dot", "Electronics", 24.99, 100), ("Juicer", "Appliances", 54.99, 30), 
+    ("Milk-Bone Dog Treats", "Pet Supplies", 10.99, 150), 
+    ("Beats Wireless On-Ear Headphones", "Electronics", 178.00, 30), 
+    ("SmartyKat Catnip Cat Toys", "Pet Supplies", 3.99, 200), 
+    ("Bose Revolve Bluetooth Speaker", "Electronics", 149.00, 70), 
+    ("Slip on Fuzzy Slippers", "Shoes", 9.99, 400), 
+    ("Anti-Slip Rain Boots", "Shoes", 24.99, 100), 
+    ("Digital Air Fryer", "Appliances", 49.99, 150), 
+    ("Crock-Pot", "Appliances", 26.99, 40), 
+    ("Indoor Rowing Machine", "Exercise & Fitness", 945.00, 10);
