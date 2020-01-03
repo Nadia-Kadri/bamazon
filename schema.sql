c2dcab4d5a9fa72a34a194bac5136d1ca48716b2
Nadia-Kadri <nadiagrace96@gmail.com>
2020-01-03 10:20:49 -0500

    schema typo

--- schema.sql
+++ schema.sql
@@ -6,9 +6,9 @@ USE bamazon;
 
 CREATE TABLE products (
   item_id INT NOT NULL AUTO_INCREMENT,
-  product_name VARCHAR(300) NULL,
-  department_name VARCHAR(300) NULL,
-  price DECIMAL(10,2) NULL,
-  stock_quantity INT NULL,
+  product_name VARCHAR(300) NOT NULL,
+  department_name VARCHAR(300) NOT NULL,
+  price DECIMAL(10,2) NOT NULL,
+  stock_quantity INT NOT NULL,
   PRIMARY KEY (item_id)
 );
